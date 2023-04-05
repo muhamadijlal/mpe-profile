5ceadfed1d6fe15e094a782a084249897b0b0215
muhamadijlal <haidarijlal027@gmail.com>
2023-04-05 13:39:48 +0700

    start commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.{html,js}"],
+  theme: {
+    fontFamily: {
+      'poppins' : ['Poppins', 'arial']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
